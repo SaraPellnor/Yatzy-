12d4d3ed84506e0161fdf3307704b38645d80674
Sara Pellnor <114143422+SaraPellnor@users.noreply.github.com>
2023-03-02 13:46:46 +0100

    starting with dices

--- script.js
+++ script.js
@@ -1,19 +1,70 @@
 "use strict";
 // Global imports from html
+const h1 = document.querySelector("h1");
+h1 === null || h1 === void 0 ? void 0 : h1.addEventListener("click", () => { returnToStart(); });
+const startContainer = document.querySelector(".startContainer--div");
 const newGameBtn = document.querySelector(".newGame--btn");
 newGameBtn.addEventListener("click", () => { gameSetup(); });
 const scoreBoardBtn = document.querySelector(".scoreBoard--btn");
 scoreBoardBtn.addEventListener("click", () => { scoreBoard(); });
+const yatzydBtn = document.querySelector(".chooseYatzy--btn");
+yatzydBtn.addEventListener("click", () => { gameBoard(); });
+const yatzyMaxidBtn = document.querySelector(".chooseMaxiYatzy--btn");
+yatzyMaxidBtn.addEventListener("click", () => { });
+const scoreKeeperDiv = document.querySelector(".scoreKeeper--div");
+const gameBoardDiv = document.querySelector(".gameBoard--div");
+// Global popup
+const popupDiv = document.createElement("div");
+popupDiv.setAttribute("class", "popup--div");
+const p = document.createElement("p");
+const popupBtnDiv = document.createElement("div");
+popupBtnDiv.setAttribute("class", "popupBtn--div");
+const popupPlayBtn = document.createElement("button");
+popupPlayBtn.setAttribute("class", "popupPlay--btn");
+popupPlayBtn.innerText = "Play";
+popupPlayBtn.addEventListener("click", () => {
+    popupDiv.style.display = "none";
+    addPlayerInput.value = "";
+    appendPlayers();
+});
+const popupReturnBtn = document.createElement("button");
+popupReturnBtn.setAttribute("class", "popupReturn--btn");
+popupReturnBtn.innerText = "Return";
+popupReturnBtn.addEventListener("click", () => {
+    popupDiv.style.display = "none";
+    addPlayerInput.value = "";
+    appendPlayers();
+});
+const appendPopup = () => {
+    popupDiv.style.display = "flex";
+    document.body.append(popupDiv);
+    popupDiv.append(p, popupBtnDiv);
+    popupBtnDiv.append(popupPlayBtn, popupReturnBtn);
+};
 // Score Board Globals
 const scoreBoardDiv = document.querySelector(".scoreBoard--div");
 const returnBtn = document.querySelector(".return--btn");
 // Game Setup Globals
 const gameSetupDiv = document.querySelector(".gameSetup--div");
 const addPlayerInput = document.querySelector(".addPlayer--input");
+// Add Player Globals
+const playerUl = document.querySelector(".player--ul");
+const players = [];
+const appendPlayers = () => players.forEach(player => {
+    const li = document.createElement("li");
+    li.innerText = player + " ";
+    playerUl.append(li);
+    li.addEventListener("click", () => { removePlayers(player); });
+});
+// Score Board
+function scoreBoard() {
+    startContainer.style.display = "none";
+    scoreBoardDiv.style.display = "flex";
+    returnBtn.addEventListener("click", () => { returnToStart(); });
+}
 // Game Setup
 function gameSetup() {
-    newGameBtn.style.display = "none";
-    scoreBoardBtn.style.display = "none";
+    startContainer.style.display = "none";
     gameSetupDiv.style.display = "flex";
     addPlayerInput.addEventListener("keydown", (event) => {
         if (event.key === 'Enter') {
@@ -21,19 +72,42 @@ function gameSetup() {
         }
     });
 }
+// Add one or more Players (max 6 players)
 function addPlayer(name) {
-    console.log(name);
+    playerUl.innerText = "";
+    if (addPlayerInput.value === "") {
+        appendPopup();
+        p.innerText = "";
+        p.innerHTML = "You dont have a name? <br> Try again pleace..";
+    }
+    else if (players.length < 6) {
+        players.push(name);
+        addPlayerInput.value = "";
+        appendPlayers();
+    }
+    else {
+        appendPopup();
+        p.innerText = "";
+        p.innerHTML = "There will be too many of you! <br> Start now instead..";
+    }
 }
-// Score Board
-function scoreBoard() {
-    newGameBtn.style.display = "none";
-    scoreBoardBtn.style.display = "none";
-    scoreBoardDiv.style.display = "block";
-    returnBtn.addEventListener("click", () => { returnToStart(); });
+// Removes player from array and ul
+function removePlayers(player) {
+    const index = players.indexOf(player);
+    players.splice(index, 1);
+    playerUl.innerText = "";
+    appendPlayers();
+}
+function gameBoard() {
+    gameSetupDiv.style.display = "none";
+    scoreKeeperDiv.style.display = "flex";
+    gameBoardDiv.style.display = "flex";
 }
 // START
 function returnToStart() {
+    gameSetupDiv.style.display = "none";
     scoreBoardDiv.style.display = "none";
-    newGameBtn.style.display = "block";
-    scoreBoardBtn.style.display = "block";
+    startContainer.style.display = "flex";
+    scoreKeeperDiv.style.display = "none";
+    gameBoardDiv.style.display = "none";
 }
--- script.ts
+++ script.ts
@@ -1,10 +1,59 @@
+
 // Global imports from html
+const h1 = document.querySelector("h1") as HTMLHeadingElement
+h1?.addEventListener("click", () => {returnToStart()})
+
+const startContainer = document.querySelector(".startContainer--div") as HTMLDivElement
+
 const newGameBtn = document.querySelector(".newGame--btn") as HTMLButtonElement
-newGameBtn.addEventListener("click",() => {gameSetup()})
+newGameBtn.addEventListener("click", () => { gameSetup() })
 
 const scoreBoardBtn = document.querySelector(".scoreBoard--btn") as HTMLButtonElement
-scoreBoardBtn.addEventListener("click",() => {scoreBoard()})
+scoreBoardBtn.addEventListener("click", () => { scoreBoard() })
 
+const yatzydBtn = document.querySelector(".chooseYatzy--btn") as HTMLButtonElement
+yatzydBtn.addEventListener("click", () => { gameBoard() })
+
+const yatzyMaxidBtn = document.querySelector(".chooseMaxiYatzy--btn") as HTMLButtonElement
+yatzyMaxidBtn.addEventListener("click", () => { /* */ })
+
+const scoreKeeperDiv = document.querySelector(".scoreKeeper--div") as HTMLDivElement
+
+const gameBoardDiv = document.querySelector(".gameBoard--div") as HTMLDivElement
+
+// Global popup
+const popupDiv = document.createElement("div")
+popupDiv.setAttribute("class", "popup--div")
+
+const p = document.createElement("p")
+
+const popupBtnDiv = document.createElement("div")
+popupBtnDiv.setAttribute("class", "popupBtn--div")
+
+const popupPlayBtn = document.createElement("button")
+popupPlayBtn.setAttribute("class", "popupPlay--btn")
+popupPlayBtn.innerText = "Play"
+popupPlayBtn.addEventListener("click", () => {
+    popupDiv.style.display = "none"
+    addPlayerInput.value = ""
+    appendPlayers()
+})
+
+const popupReturnBtn = document.createElement("button")
+popupReturnBtn.setAttribute("class", "popupReturn--btn")
+popupReturnBtn.innerText = "Return"
+popupReturnBtn.addEventListener("click", () => {
+    popupDiv.style.display = "none"
+    addPlayerInput.value = ""
+    appendPlayers()
+})
+
+const appendPopup = () => {
+    popupDiv.style.display = "flex"
+    document.body.append(popupDiv)
+    popupDiv.append(p, popupBtnDiv)
+    popupBtnDiv.append(popupPlayBtn, popupReturnBtn)
+}
 
 // Score Board Globals
 const scoreBoardDiv = document.querySelector(".scoreBoard--div") as HTMLDivElement
@@ -14,10 +63,28 @@ const returnBtn = document.querySelector(".return--btn") as HTMLButtonElement
 const gameSetupDiv = document.querySelector(".gameSetup--div") as HTMLDivElement
 const addPlayerInput = document.querySelector(".addPlayer--input") as HTMLInputElement
 
+// Add Player Globals
+const playerUl = document.querySelector(".player--ul") as HTMLDivElement
+const players: string[] = []
+
+const appendPlayers = () => players.forEach(player => {
+    const li = document.createElement("li") as HTMLLIElement
+    li.innerText = player + " "
+    playerUl.append(li)
+    li.addEventListener("click", () => { removePlayers(player) })
+});
+
+// Score Board
+function scoreBoard() {
+    startContainer.style.display = "none"
+    scoreBoardDiv.style.display = "flex"
+
+    returnBtn.addEventListener("click", () => { returnToStart() })
+}
+
 // Game Setup
 function gameSetup() {
-    newGameBtn.style.display = "none"
-    scoreBoardBtn.style.display = "none"
+    startContainer.style.display = "none"
     gameSetupDiv.style.display = "flex"
 
     addPlayerInput.addEventListener("keydown", (event) => {
@@ -26,24 +93,50 @@ function gameSetup() {
         }
     })
 }
+// Add one or more Players (max 6 players)
+function addPlayer(name: string) {
+    playerUl.innerText = ""
 
-function addPlayer(name: String) {   //<-------------make li for each name in hard koded ul
-    console.log(name);
+    if (addPlayerInput.value === "") {
+        appendPopup()
+        p.innerText = ""
+        p.innerHTML = "You dont have a name? <br> Try again pleace.."
+    } else if (players.length < 6) {
+        players.push(name)
+        addPlayerInput.value = ""
+        appendPlayers()
+    } else {
+        appendPopup()
+        p.innerText = ""
+        p.innerHTML = "There will be too many of you! <br> Start now instead.."
+    }
+}
+
+// Removes player from array and ul
+function removePlayers(player: string) {
+    const index = players.indexOf(player)
+    players.splice(index, 1)
+    playerUl.innerText = ""
+    appendPlayers()
+}
+
+function gameBoard() {
+    gameSetupDiv.style.display = "none"
+    scoreKeeperDiv.style.display = "flex"
+    gameBoardDiv.style.display = "flex"
     
+
 }
 
-// Score Board
-function scoreBoard() {
-    newGameBtn.style.display = "none"
-    scoreBoardBtn.style.display = "none"
-    scoreBoardDiv.style.display = "block"
 
-    returnBtn.addEventListener("click", () => {returnToStart()})
-}
+
 
 // START
-function returnToStart () {
+function returnToStart() {
+    gameSetupDiv.style.display = "none"
     scoreBoardDiv.style.display = "none"
-    newGameBtn.style.display = "block"
-    scoreBoardBtn.style.display = "block"
+    startContainer.style.display = "flex"
+    scoreKeeperDiv.style.display = "none"
+    gameBoardDiv.style.display = "none"
+
 }
